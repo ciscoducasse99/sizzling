4d6e6a9cbfb1d4d28ec713dce03bd256c403cb19
ciscoducasse99 <ciscoducasse99@hotmail.com>
2022-02-08 11:47:29 -0500

    server

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 db.sequelize
   .sync()
   .then(() => {
-    console.log("> DB connected");
+    console.log(">DB connected");
   })
   .then(() => {
     app.listen(PORT, () => {
